1d856ffd4ca9e56ed4e252bed0b07178002fb79c
Dmitry Marov <d.marov94@gmail.com>
2018-01-14 20:31:39 +0300

    guake

--- guake.zsh
+++ guake.zsh
@@ -3,10 +3,10 @@
 guake &;
 sleep 30;
 
-guake -n 'nvim';
-guake -e 'nvim';
+#guake -n 'nvim';
+#guake -e 'nvim';
 
-guake -n 'vifm';
-guake -e 'vifm';
+#guake -n 'vifm';
+#guake -e 'vifm';
 
 guake -s 0;
